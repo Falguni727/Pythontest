8353a336ff6fd0b0d263942081fc105a888ac90c
Falguni727 <falgunibhalala@yahoo.com>
2026-05-09 17:55:09 +0530

    Patched changes

--- nain.py
+++ nain.py
@@ -1,1 +1,15 @@
-print("Main")
+data = dict({
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York",
+    "occupation": "Software Engineer",
+    "salary": 80000.00
+})
+
+def main():
+    print("Incoming Data...")
+    print(data["salary"])
+    print(type(data["salary"]))
+
+
+main()
